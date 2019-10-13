3ca403e381980b483763d34484fd68c26c43626c
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-13 20:03:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,10 +20,10 @@ class Transfer
        return "Transaction rejected. Please check your account balance."
     elsif @status == "complete"
       puts "Transaction was already excuted"
-   # else
-   #   @sender.deposit( @amount * -1 ) 
-   #   @receiver.deposit( @amount )
-   #   @status = "complete"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
     end
   end
 
