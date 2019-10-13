e44adbfa37ce8f694624253f5f0967d8720a4fa9
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-13 20:10:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,12 +28,12 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status == "complete"
-      @sender.deposit(@amount) 
-      @receiver.deposit(@amount * -1)
-      @status = "reversed"
-    end
-  end
+ #   if @status == "complete"
+ #     @sender.deposit(@amount) 
+ #     @receiver.deposit(@amount * -1)
+ #     @status = "reversed"
+ #   end
+ # end
   
     if self.valid? && self.status == "complete" && @sender.balance > @amount
       @sender.balance += @amount
