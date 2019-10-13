16b9560143900833f68b712da1a19a7719068807
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-13 19:57:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,12 +14,26 @@ class Transfer
     self.sender.valid? && self.receiver.valid? && @amount <= self.sender.balance
   end
 
-  def execute_transaction
-    @status = "complete"
+def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end
   end
-  
+
   def reverse_transfer
-    
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
   end
   
 end
